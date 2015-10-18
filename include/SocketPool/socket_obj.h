60e9552bde25b78784b3c0b5fc61e1ca80ecfb9c
Xiong Jun <adairjun@gmail.com>
2015-10-18 23:47:35 +0800

    BUG: not finish just_test.cpp

--- include/SocketPool/socket_obj.h
+++ include/SocketPool/socket_obj.h
@@ -11,6 +11,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+class SocketObj {
+ public:
+  SocketObj();
+  virtual ~MysqlObj();
+ private:
 
+}
+
+
+// 使用shared_ptr来替换SocketObj*
+typedef boost::shared_ptr<SocketObj>SocketObjPtr;
 #endif /* SOCKETPOOL_INCLUDE_SOCKET_OBJ_H */
 
