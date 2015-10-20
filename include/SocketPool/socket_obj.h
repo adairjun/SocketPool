a934c7a6624928821356cd8c92174aa261022fc2
Xiong Jun <adairjun@gmail.com>
2015-10-20 23:58:24 +0800

    BUG:a bug in socket_obj.cpp

--- include/SocketPool/socket_obj.h
+++ include/SocketPool/socket_obj.h
@@ -3,37 +3,61 @@
 // Author: adairjun
 // This is used to construct a socket connection obj 
 
-
 #ifndef SOCKETPOOL_INCLUDE_SOCKET_OBJ_H
 #define SOCKETPOOL_INCLUDE_SOCKET_OBJ_H
 
+#include <string>
 #include <boost/shared_ptr.hpp>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+using std::string;
 
-#include <event2/event.h>
-#include <event2/bufferevent.h>
-#include <event2/buffer.h>
+class SocketObj;
 
-#ifdef __cplusplus
-}
-#endif
+// 使用shared_ptr来替换SocketObj*
+typedef boost::shared_ptr<SocketObj>SocketObjPtr;
 
 class SocketObj {
  public:
   SocketObj();
   virtual ~SocketObj();
- private:
+  void Dump() const;
 
+  // 由于sockaddr_in.sin_addr.s_addr类型是unsigned类型
+  // 这个函数的作用就是把ip地址转换成s_addr能接受的类型
+  // 方便构建sockaddr_in
+  unsigned TranslateAddress();
+  
+  // 封装的Bind函数
+  int Bind();
+
+  //封装的listen函数
+  int Listen();
+
+  //封装的accept函数,由于accept函数返回一个套接字,这里再做一层封装,根据返回的套接字构建SocketObj对象 
+  SocketObjPtr Accept();
+   
+  //封装的connect函数  
+  int Connect();
+
+  //封装的close函数
+  int Close();
+
+  //获取套接字
+  int Get() const {
+    return m_sockFD;
+  }
+
+ private:
+  // 通过socket函数构建的套接字
+  int m_sockFD;
+ 
+  struct sockaddr_in m_sAddr; 
+  string m_strHost;
+  unsigned m_iPort;
+  
+  //用于listen函数的backlog,决定内核为socket排队的最大连接个数
+  //从配置文件当中读取
+  int backlog;
 };
 
-
-// 使用shared_ptr来替换SocketObj*
-typedef boost::shared_ptr<SocketObj>SocketObjPtr;
 #endif /* SOCKETPOOL_INCLUDE_SOCKET_OBJ_H */
-
--- util/socket_obj.cpp
+++ util/socket_obj.cpp
@@ -1,2 +1,111 @@
 #include "SocketPool/socket_obj.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
+SocketObj::SocketObj(string host, unsigned port)
+    : m_strHost(host),
+      m_iPort(port) {
+    m_sockFD = -1;
+} 
+
+SocketObj::SocketObj(int sockFD)
+    : m_sockFD(sockFD) {
+}
+
+SocketObj::~SocketObj() {
+  Close();
+}
+
+void SocketObj::Dump() const {
+  printf("m_sockFD=%d", m_sockFD);
+}
+
+unsigned SocketObj::TranslateAddress() {
+  if (m_strHost == "")
+    return INADDR_ANY; 
+  
+  // 建立一个临时的socket
+  int tmpSockFD = socket(AF_INET, SOCK_DGRAM, 0);
+  // ifreq这个结构体的用处用来配置ip地址,激活接口,配置MTU等信息的
+  struct ifreq sIfReq;
+  strcpy(sIfReq.ifr_name, m_strHost.c_str() + 1);
+  strcpy(sIfReq.ifr_name, m_strHost.c_str() + 1);
+  if (0 != ioctl(tmpSockFD, SIOCGIFADDR, &sIfReq, sizeof(sIfReq))) {
+    close(tmpSockFD); 
+	return INADDR_NONE;
+  }
+  close(tmpSockFD); 
+  struct sockaddr_in *pAddr = (struct sockaddr_in *)&(sIfReq.ifr_addr);
+  return pAddr->sin_addr.s_addr;
+}
+
+int SocketObj::Bind() {
+  Close();
+  m_sockFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (m_sockFD == -1) {
+    return -1;
+  }
+  
+  memset(&m_sAddr, 0, sizeof(m_sAddr));
+  m_sAddr.sin_addr.s_addr = TranslateAddress();
+  m_sAddr.sin_family = AF_INET;
+  m_sAddr.sin_port = htons(m_iPort);
+  return bind(m_sockFD, (struct sockaddr*)&m_sAddr, sizeof(m_sAddr)); 
+}
+
+int SocketObj::Listen() {
+  Close();
+  Bind();
+  if (Bind() != 0) {
+    Close();
+    return -1;
+  }
+
+  if (listen(m_sockFD, backlog) != 0) {
+    Close();
+    return -1;
+  }
+  return 0;
+}
+
+SocketObjPtr SocketObj::Accept() {
+  if (m_sockFD == -1) {
+    return NULL;
+  }
+  socklen_t length = sizeof(m_sAddr);
+  int customFD = accept(m_sockFD, (struct sockaddr*)&m_sAddrLen, &length) 
+  if (customFD == -1) {
+    return NULL;
+  }
+  boost::shared_ptr<SocketObj> tPtr(new SocketObj(customFD));
+  return tPrt;
+}
+
+int SocketObj::Connect() {
+  Close();
+  m_sockFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (m_sockFD == -1) {
+    return -1;
+  }
+
+  memset(&m_sAddr, 0, sizeof(m_sAddr));
+  m_sAddr.sin_addr.s_addr = TranslateAddress(); 
+  m_sAddr.sin_family = AF_INET;
+  m_sAddr.sin_port = htons(m_iPort);
+  if (connect(m_sockFD, (struct sockaddr*)&m_sAddr, sizeof(m_sAddr))!=0) {
+    Close();
+    return -1;
+  }
+  return 0; 
+}
+
+int SocketObj::Close() {
+  if (m_sockFD != -1) {
+    close(m_sockFD);
+    m_sockFD = -1;
+  }
+}
